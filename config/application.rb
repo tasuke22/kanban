242ea98ef1b77d741929aa9a66e26ce4a81afd1a
EC2 Default User <ec2-user@ip-172-31-35-214.ap-southeast-1.compute.internal>
2018-11-09 18:29:44 +0000

    3

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,13 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Kanban
+module Kanban2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+    # ここに追加する
+    config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
